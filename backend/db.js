86f87163c82c39da1db431cd021706e3e0d361f3
Aranya Sen <149432679+Aranya-Sen@users.noreply.github.com>
2026-04-11 12:52:10 +0530

    Refactor database connection configuration

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "school_db",
-  password: process.env.DB_PASSWORD || "Riju2004",
-  port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER ,
+  host: process.env.DB_HOST ,
+  database: process.env.DB_NAME ,
+  password: process.env.DB_PASSWORD ,
+  port: process.env.DB_PORT ,
 });
 
 const initDB = async () => {
