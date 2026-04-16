8b44975056b60604f8f4e4a26390ea7fa78a89da
mohammadshezan <164716637+mohammadshezan@users.noreply.github.com>
2026-04-16 13:08:36 +0530

    Update mailer.js to use EMAIL_USER and EMAIL_PASS

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS   // App password (NOT your real password)
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS   // App password (NOT your real password)
   }
 });
 
